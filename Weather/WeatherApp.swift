ecf612938c2fe8a7613eaf1711a2e813cb9294d0
Bruno Ndiba Mbwaye Roy <brunombw@seas.upenn.edu>
2025-04-09 16:18:54 -0400

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Bruno Ndiba Mbwaye Roy on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
